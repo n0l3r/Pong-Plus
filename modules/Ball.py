76e95b8e692ae8d7a3d53ba92996fa7f55b6befc
Yusuf Hafidz <yhafidz@gmail.com>
2022-04-17 15:39:52 +0700

    New modules, Ball.py

---
+++ modules/Ball.py
@@ -0,0 +1,14 @@
+class Ball():
+    def __init__(self, image, size, vec_x, vec_y, pos_x, pos_y) -> None:
+        self.image= image
+        self.size = size
+        self.vec_x = vec_x
+        self.vec_y = vec_y
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+    
+    def move():
+        pass
+
+    def show():
+        pass
