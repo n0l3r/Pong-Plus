58882d2fc08bb662b5a92172c1fed5f0c806c782
maskods <naufal.120140044@student.itera.ac.id>
2022-04-25 01:05:46 +0700

    update class menu

--- modules/Menu.py
+++ modules/Menu.py
@@ -1,17 +1,11 @@
 from abc import ABC, abstractmethod
-import pygame
+import pygame, sys
 from modules.Button import Button
 
 TITLE = "Pong Plus"
 
 MENU_BG = (8, 32, 50)
 GAME_BG = (1, 0, 6)
-
-PLAY_BTN_IMG = pygame.image.load("assets/main_menu/play_btn.png")
-ABOUT_BTN_IMG = pygame.image.load("assets/main_menu/about_btn.png")
-EXIT_BTN_IMG = pygame.image.load("assets/main_menu/exit_btn.png")
-
-
 class Menu(ABC):
     def __init__(self, name):
         self.name = name
@@ -21,41 +15,87 @@ class Menu(ABC):
         pass
 
 
-class MainMenu(Menu):
+class Main(Menu):
     def __init__(self):
-        super().__init__("Main Menu")
+        super().__init__("main_menu")
 
     def render(self, screen):
         screen.fill(MENU_BG)
 
-        logo_pbb = pygame.image.load("assets/main_menu/logo_pbb.png")
+        logo_pbb = pygame.image.load("assets/images/logo_pbb.png")
         logo_pbb.set_alpha(255)
         screen.blit(logo_pbb, (15, 15))
 
-        info_btn = pygame.image.load("assets/main_menu/info_btn.png")
-        info_btn.set_alpha(255)
-        screen.blit(info_btn, (1220, 15))
-
-        logo_img = pygame.image.load("assets/main_menu/Pong_Logo.png")
+        logo_img = pygame.image.load("assets/images/Pong_Logo.png")
         logo_img.set_alpha(255)
         screen.blit(logo_img, (494, 173))
 
-        play_btn = Button(image=PLAY_BTN_IMG, pos=(505, 400))
-        play_btn.render(screen)
+        info_img = pygame.image.load("assets/button/info_btn.png")
+        self.info_btn = Button(image=info_img, pos=(1220, 15))
+        self.info_btn.render(screen)
 
-        about_btn = Button(ABOUT_BTN_IMG, (505, 465))
-        about_btn.render(screen)
+        play_img = pygame.image.load("assets/button/play_btn.png")
+        self.play_btn = Button(image=play_img, pos=(505, 400))
+        self.play_btn.render(screen)
 
-        exit_btn = Button(EXIT_BTN_IMG, (505, 530))
-        exit_btn.render(screen)
+        about_img = pygame.image.load("assets/button/about_btn.png")
+        self.about_btn = Button(image=about_img, pos=(505, 465))
+        self.about_btn.render(screen)
 
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+        exit_img = pygame.image.load("assets/button/exit_btn.png")
+        self.exit_btn = Button(image=exit_img, pos=(505, 530))
+        self.exit_btn.render(screen)
 
-            pygame.display.update()
+        
 
+
+class About(Menu):
+    def __init__(self):
+        super().__init__("about_menu")
+
+    def render(self, screen):
+        screen.fill(MENU_BG)
+
+        logo_pbb = pygame.image.load("assets/images/logo_pbb.png")
+        logo_pbb.set_alpha(255)
+        screen.blit(logo_pbb, (15, 15))
+
+        logo_img = pygame.image.load("assets/images/Pong_Logo.png")
+        logo_img.set_alpha(255)
+        screen.blit(logo_img, (494, 173))
+        
+        about_text = pygame.image.load("assets/images/about_text.png")
+        about_text.set_alpha(255)
+        screen.blit(about_text, (215, 125))
+
+        back_img = pygame.image.load("assets/button/back_btn.png")
+        self.back_btn = Button(image=back_img, pos=(505, 575))
+        self.back_btn.render(screen)
+
+class Info(Menu):
+    def __init__(self):
+        super().__init__("info_menu")
+
+    def render(self, screen):
+        screen.fill(MENU_BG)
+
+        logo_pbb = pygame.image.load("assets/images/logo_pbb.png")
+        logo_pbb.set_alpha(255)
+        screen.blit(logo_pbb, (15, 15))
+
+        logo_img = pygame.image.load("assets/images/Pong_Logo.png")
+        logo_img.set_alpha(255)
+        screen.blit(logo_img, (494, 173))
+
+        info_text = pygame.image.load("assets/images/info_text.png")
+        info_text.set_alpha(255)
+        screen.blit(info_text, (215, 125))
+
+        back_img = pygame.image.load("assets/button/back_btn.png")
+        self.back_btn = Button(image=back_img, pos=(505, 575))
+        self.back_btn.render(screen)
+
+
+       
 
         
