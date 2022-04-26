37780993b9c016d78b9bfa33981822e14abe7674
AfifSaifullah <afifsyaifullahfattah@gmail.com>
2022-04-26 15:40:26 +0700

    update paddle

--- modules/Paddle.py
+++ modules/Paddle.py
@@ -1,12 +1,16 @@
 class Paddle:
-    def __init__(self, image, side, length, pos_y):
+    def __init__(self, image, side, length, pos_x, pos_y, speed):
         self.image = image
-        self.side = side
+        self.side = side # 0 = kiri, 1 = kanan
         self.lenght = length
+        self.pos_x = pos_x
         self.pos_y = pos_y
+        self.speed = speed # Kecepatan gerak
+
 
     def move(self):
         pass
+
 
     def render(self):
         pass
