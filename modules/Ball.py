d304a3cccd31cdceb87ad286d90bdbdef9aaaa28
AfifSaifullah <afifsyaifullahfattah@gmail.com>
2022-05-11 00:35:13 +0700

    update ball

--- modules/Ball.py
+++ modules/Ball.py
@@ -1,10 +1,11 @@
 from modules.Paddle import Paddle
+import pygame
 import math
 
 
 class Ball():
-    def __init__(self, image, size, vec_x, vec_y, pos_x, pos_y) -> None:
-        self.image= image
+    def __init__(self, image:pygame.Surface, size, vec_x, vec_y, pos_x, pos_y) -> None:
+        self.image = image
         self.size = size
         self.vec_x = vec_x
         self.vec_y = vec_y
@@ -23,12 +24,12 @@ class Ball():
         
         else: # Arah pantulan bola bervariasi berdasarkan jarak bola dari titik tengah paddle
             focal_point = 2*(paddle.length if paddle.side else -paddle.length)
-            height_diff = (self.pos_y + self.size)/2 - (paddle.pos_y + paddle.length)/2
+            height_diff = (self.pos_y + self.size/2) - (paddle.pos_y + paddle.length/2)
             angle = math.atan(focal_point/height_diff)
 
             self.vec_x = self.vec_x*math.cos(2*angle) + self.vec_y*math.sin(2*angle)
             self.vec_y = self.vec_x*math.sin(2*angle) - self.vec_y*math.cos(2*angle)
         
 
-    def render(self):
-        pass
+    def render(self, screen):
+        screen.blit(self.image, (self.pos_x, self.pos_y))
