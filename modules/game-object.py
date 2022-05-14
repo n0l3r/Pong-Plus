7ccd4283d1f2f73658d5d9f9189156d674b38737
Yusuf Hafidz <yhafidz@gmail.com>
2022-05-14 22:56:49 +0700

    new file: game-object.py

---
+++ modules/game-object.py
@@ -0,0 +1,24 @@
+import re
+import pygame
+
+class Game_Object(pygame.sprite.Sprite):
+    def __init__(self, rect:pygame.Rect ) -> None:
+        super().__init__()
+        self.rect = rect
+        self._x = float(rect.x)
+        self._y = float(rect.y)
+    
+    @property
+    def x(self):
+        return self._x
+    @x.setter
+    def x(self, val:float):
+        self._x = val
+        self.rect.x = int(self._x)
+    @property
+    def y(self):
+        return self._y
+    @y.setter
+    def y(self, val:float):
+        self._y = val
+        self.rect.y = int(self._y)
