869cc882d07f4774c2c88098744ca09ec97c2dd6
Yusuf Hafidz <yhafidz@gmail.com>
2022-05-15 05:53:31 +0700

    base file main_2.py

---
+++ main_2.py
@@ -0,0 +1,20 @@
+# Modules
+import pygame
+from modules.Game import Game
+
+# PYGAME INIT
+pygame.init()
+fps_clock = pygame.time.Clock()
+
+# Display Setting
+SIZE = (1280, 720)
+icon = pygame.image.load("assets/images/icon.png")
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("Pong Plus")
+pygame.display.set_icon(icon)
+
+g = Game(screen, fps_clock)
+
+# Mainloop
+while True:
+    pass
---
+++ modules/Game.py
@@ -0,0 +1,9 @@
+import pygame
+
+
+class Game:
+    def __init__(self, screen:pygame.Surface, clock:pygame.time.Clock) -> None:
+        pass
+
+    def play():
+        pass
