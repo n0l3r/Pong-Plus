319c3d005254e3b52be02a54e33611cdc9cf7a42
Yusuf Hafidz <yhafidz@gmail.com>
2022-05-14 23:17:33 +0700

    comment

--- modules/game-object.py
+++ modules/game-object.py
@@ -1,9 +1,15 @@
 import pygame
 
 class Game_Object(pygame.sprite.Sprite):
+    """
+    Class (turunan dari pygame.sprite.Sprites) untuk semua in-game object.
+    Constructor mengambil satu argumen yaitu rect (pygame.Rect) dari objeknya
+    Punya getter dan setter sendiri buat nentuin posisi (x,y).
+    """
     def __init__(self, rect:pygame.Rect ) -> None:
         super().__init__()
         self.rect = rect
+        # Float untuk presisi posisi
         self._x = float(rect.x)
         self._y = float(rect.y)
     
