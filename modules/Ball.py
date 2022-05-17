2fa907336b42489752edd8bcd0e5f748351c5e47
AfifSaifullah <afifsyaifullahfattah@gmail.com>
2022-05-18 02:53:32 +0700

    update ball

--- modules/Ball.py
+++ modules/Ball.py
@@ -1,24 +1,24 @@
 from modules.Paddle import Paddle
 from modules.GameObject import GameObject
 import pygame
-from math import atan, sin, cos, sqrt
+from math import atan, sin, cos, radians, hypot
 
 # Neon pada image ball
 BALL_NEON = 15
 
 class Ball(GameObject):
-    def __init__(self, image:pygame.Surface, vec_x, vec_y, x, y) -> None:
+    def __init__(self, image:pygame.Surface, x, y, speed, angle) -> None:
         self.size = 50
         self.image = pygame.transform.scale(image, [self.size + 28, self.size + 28])
 
         super().__init__(pygame.rect.Rect(0, 0, self.size, self.size))
 
-        self.vec_x = vec_x
-        self.vec_y = vec_y
+        self.speed = speed
+        self.vec_x = speed*cos(radians(angle))
+        self.vec_y = speed*sin(radians(angle))
         self.x = x - self.size/2
         self.y = y - self.size/2
-        self.velocity = sqrt(vec_x**2 + vec_y**2)
-
+        
 
     def move(self):
         self.x += self.vec_x
@@ -30,7 +30,6 @@ class Ball(GameObject):
             self.vec_y *= -1
         
         else: # Arah pantulan bola bervariasi berdasarkan jarak bola dari titik tengah paddle
-            focal_point = 2*(paddle.height if paddle.side == 0 else -paddle.height)
             height_diff = self.rect.centery - paddle.rect.centery
 
             if height_diff == 0:
@@ -38,28 +37,38 @@ class Ball(GameObject):
                 self.move()
                 return
 
-            angle = atan(focal_point/height_diff) # Sudut bidang pantul
+            focal_point = 2*(-paddle.height if paddle.side else paddle.height)
+            ref_angle = atan(focal_point/height_diff) # Sudut bidang pantul
 
-            self.vec_x = self.vec_x*cos(2*angle) + self.vec_y*sin(2*angle)
-            self.vec_y = self.vec_x*sin(2*angle) - self.vec_y*cos(2*angle)
+            self.vec_x = self.vec_x*cos(2*ref_angle) + self.vec_y*sin(2*ref_angle)
+            self.vec_y = self.vec_x*sin(2*ref_angle) - self.vec_y*cos(2*ref_angle)
 
-            # Sedikit perbaikan sementara untuk bug kecepatan bola yang entah kenapa melambat ketika memantul
-            vel_change = self.velocity/sqrt(self.vec_x**2 + self.vec_y**2)
-            self.vec_x *= vel_change
-            self.vec_y *= vel_change
+            slope = self.vec_y/self.vec_x
+            max_angle = radians(60)
 
-            v_tan = self.vec_y/self.vec_x # nilai tan vektor akhir bola
+            # Membatasi sudut maksimal bola bergerak
+            if slope > max_angle or slope < -max_angle:
+                self.vec_x = (-1 if paddle.side else 1)*self.speed*cos(max_angle)
+                self.vec_y = (-1 if self.vec_y < 0 else 1)*self.speed*sin(max_angle)
 
-            # Jika v_tan lebih besar dari 1.7 (tan(60 derajat)) maka sudut vektor diubah menjadi 60
-            # untuk mencegah sudut bola menjadi vertikal
-            if v_tan > 1.7 or v_tan < -1.7:
-                v_tan = 1.7
-                new_angle = atan(v_tan)
+            # Mereset kecepatan bola jika berbeda dari kecepatan asli
+            new_speed = hypot(self.vec_x, self.vec_y)
+            self.vec_x *= self.speed/new_speed
+            self.vec_y *= self.speed/new_speed
 
-                self.vec_x = (-1 if paddle.side else 1)*self.velocity*cos(new_angle)
-                self.vec_y = (-1 if self.vec_y < 0 else 1)*self.velocity*sin(new_angle)
+            self.move()
 
-        self.move()
+
+    def check_collision(self, top_boundary, bottom_boundary, left_paddle, right_paddle):
+        # Cek collision bola dengan tembok atas dan bawah
+        if self.rect.top <= top_boundary or self.rect.bottom >= bottom_boundary:
+            self.bounce(0)
+
+        # Cek collision bola dengan paddle
+        if self.rect.colliderect(left_paddle.rect):
+            self.bounce(1, left_paddle)
+        elif self.rect.colliderect(right_paddle.rect):
+            self.bounce(1, right_paddle)
         
 
     def render(self, screen):
