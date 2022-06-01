d9c8eb940dfe40d9bd3adf08058d5cef77b109c8
AfifSaifullah <afifsyaifullahfattah@gmail.com>
2022-06-01 23:09:39 +0700

    update main

--- modules/Game.py
+++ modules/Game.py
@@ -171,7 +171,7 @@ class Game:
 
             self.screen.blit(game_screen, [board.x + 30, board.y + 30])
             pygame.display.update([board.x, 0, board.width + 55, 720])
-            self.clock.tick(self.FPS)
+            # self.clock.tick(self.FPS)
 
             # Sementara exit setelah menang (Nanti diubah ketika halaman pemenang sudah dibuat)
             if player_left.score >= self.max_score or player_right.score >= self.max_score:
